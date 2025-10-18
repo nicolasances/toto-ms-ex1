ca4407713db216ed6682d377382d1008893299fe
nicolasances <nicolas.matteazzi@gmail.com>
2025-10-18 18:37:47 +0200

    API Controller 13.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { TotoAPIController } from "toto-api-controller";
 import { ControllerConfig } from "./Config";
 import { SayHello } from "./dlg/Hello";
-import { Smoke } from "./dlg/Smoke";
 
-const api = new TotoAPIController("toto-ms-ex1", new ControllerConfig())
+const api = new TotoAPIController("toto-ms-ex1", new ControllerConfig(), { basePath: '/ex1' });
 
-api.path('GET', '/health', new Smoke(), { noAuth: true, contentType: 'application/json' });
-
-api.path('GET', '/ex1/hello', new SayHello());
+api.path('GET', '/hello', new SayHello());
 
 api.init().then(() => {
-    api.listen()
+    api.listen();
 });
